9e591ea2723b204a1f6501b40154842a12e9e809
tass0523 <amtsk2023@outlook.jp>
2024-06-29 15:59:19 +0900

    Issue4~6-2

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,14 +34,8 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-     
-      wdays_num = #wdayメソッドを用いて使用する数値
 
-      if #wdays_numが7以上の場合
-        wdays_num = wdays_num -7
-      end
-
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wdays => (wdays_num -7) }
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, }
       @week_days.push(days)
     end
   end
